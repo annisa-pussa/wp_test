9fe1fda2c814be894294a2aecf6de6311d433b8d
annisaps313 <annisaps313@gmail.com>
2026-04-19 15:27:44 +0700

    Add themes

--- wp-content/themes/twentytwentyfive/functions.php
+++ wp-content/themes/twentytwentyfive/functions.php
@@ -164,3 +164,10 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
+
+add_action( 'after_setup_theme', 'wp_learn_setup_theme');
+
+function wp_learn_setup_theme() {
+    add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
+}
+
